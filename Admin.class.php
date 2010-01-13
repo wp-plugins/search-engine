09753462c9a9dcd72e4420924caad6d8d97e5ef8
sc0ttkclark <sc0ttkclark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-13 04:00:58 +0000

    implementing file structure
    git-svn-id: https://plugins.svn.wordpress.org/search-engine/trunk@193476 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Admin.class.php
@@ -0,0 +1,5 @@
+<?php
+class Search_Engine_Admin
+{
+
+}
---
+++ search-engine.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Plugin Name: Search Engine
+Plugin URI: http://www.scottkclark.com/
+Description: Currently in development - Not intended for download and use on LIVE sites! - A search engine for WordPress that indexes pages on a site and provides comprehensive functionality.
+Version: 0.0.0
+ Author: sc0ttkclark, cadillacandy
+ Author URI: http://www.scottkclark.com/
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
