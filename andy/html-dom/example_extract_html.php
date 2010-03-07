71c117c83593dda41028326307896e77044373ae
sc0ttkclark <sc0ttkclark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-07 09:12:56 +0000

    Initial Release! 0.4.0 - now with Ricky Martin soundtrack!
    git-svn-id: https://plugins.svn.wordpress.org/search-engine/trunk@214497 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- andy/html-dom/example_extract_html.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once('simple_html_dom.php');
-
-
-echo file_get_html('http://andyweigel.com/blog/wordpress-plugins/6-must-have-plugins-for-your-wordpress-installs/30')->plaintext;
-?>
